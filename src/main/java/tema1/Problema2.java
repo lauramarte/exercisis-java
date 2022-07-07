fdc8d6801e532c5c78153c493185475d55d2e1e5
laura <lauramartemarte@gmail.com>
2022-07-07 14:00:46 +0200

    .

---
+++ src/main/java/tema1/Problema2.java
@@ -0,0 +1,10 @@
+package tema1;
+
+public class Problema2 {
+
+    public static void main(String args[]){
+
+        System.out.println("Bon dia");
+
+    }
+}
---
+++ src/main/java/tema1/Problema3.java
@@ -0,0 +1,16 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class Problema3 {
+
+    public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriu un valor: ");
+
+        int number = scanner.nextInt();
+
+        System.out.println("El valor es: " + number);
+    }
+}

9612f3e60b22959e3f344731b65afdb3022cd7a4
laura <lauramartemarte@gmail.com>
2022-07-19 15:13:29 +0200

    .

---
+++ src/main/java/tema3/Problema16.java
@@ -0,0 +1,18 @@
+package tema3;
+
+import java.util.Scanner;
+
+public class Problema16 {
+
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Write first number: ");
+
+		int num1 = scanner.nextInt();
+
+		System.out.println("Enter second number: ");
+		int num2 = scanner.nextInt();
+
+	}
+}
